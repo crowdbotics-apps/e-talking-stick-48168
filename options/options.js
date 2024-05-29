8bdaad82c2a52fd2d6313d2e09f4486fd7d18547
Crowdbotics <team@crowdbotics.com>
2024-05-29 21:11:43 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "e-talking-stick-48168",
+  url: "https://e-talking-stick-48168.botics.co",
+  api: "https://e-talking-stick-48168.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
